be45cc2135192c1e1c31bd12fde9a09beffd5e0c
Viktoriia Melnyk <melnychka1993@gmail.com>
2025-11-24 16:04:13 +0000

    Integrated SheCodes AI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const form = document.getElementById('poem-form');
 
-function generatePoem(event) {
-	event.preventDefault();
-
+const displayPoem = response => {
 	new Typewriter('#poem', {
-		strings: 'Here is your generated poem based on the theme you provided.',
+		strings: `${response.data.answer}`,
 		autoStart: true,
 		delay: 1,
 		cursor: '',
 	});
+};
+
+function generatePoem(event) {
+	event.preventDefault();
+
+	const themeInput = document.getElementById('theme-input').value;
+	const API_KEY = 'aao33d4100dc1f18c42d1b9teb580408';
+	const prompt = `Generate a poem about ${themeInput}`;
+	const context =
+		'You are a creative poet who writes beautiful and imaginative poems. Your poems are short, up to 6 lines long. Make sure the poem corresponds to the given theme. Each line is an HTML paragraph.';
+	const API_URL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${API_KEY}`;
+	axios.get(API_URL).then(displayPoem);
 }
 
 form.addEventListener('submit', generatePoem);
